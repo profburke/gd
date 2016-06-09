d7e9691e561f4710a536b832e3aa2f7c0a275afc
Matthew Burke <matthew.burke@capitalone.com>
2016-06-09 15:25:57 +0000

    changes

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Gd",
     dependencies: [
-		.Package(url: "https://bitbucket.org/bluedino/cgd.git", majorVersion: 1)
+		.Package(url: "https://bitbucket.org/bluedino/cgd.git", majorVersion: 1, minor: 1)
         ]
     )
--- Source/Gd.swift
+++ Source/Gd.swift
@@ -1,4 +1,8 @@
+import Foundation
 import CGd
+
+public typealias Byte = UInt8
+
 
 public class Gd {
 
@@ -10,11 +14,11 @@ public class Gd {
     let red: Int32
     let yellow: Int32
 
-    // var lineThickness: Int32 = 1 {
-    //     didSet {
-    //         gdImageSetThickness(im, lineThickness)
-    //     }
-    // }
+    public var lineThickness: Int32 = 1 {
+        didSet {
+            gdImageSetThickness(im, lineThickness)
+        }
+    }
     
     public init(width: Int32, height: Int32) {
         self.width = width
@@ -26,29 +30,28 @@ public class Gd {
         yellow = gdImageColorAllocate(im, 255, 255, 0)
     }
 
-    // public func drawArc(cx: Int32, cy: Int32, w: Int32, h: Int32,
-    //                s: Int32, e: Int32, color: Int32 = red) {
-    //     gdImageArc(im, cx, cy, w, h, s, e, color)
-    // }
+    public func drawArc(cx: Int32, cy: Int32, w: Int32, h: Int32,
+                   s: Int32, e: Int32, color: Int32) {
+        gdImageArc(im, cx, cy, w, h, s, e, color)
+    }
 
-    // public func drawString(text: String, x: Int32, y: Int32, color: Int32 = yellow) {
-    //     let cString = text.cString(using: NSASCIIStringEncoding)!
-    //     let cPtr = UnsafeMutablePointer<UInt8>(cString)
-    //     gdImageString(im, gdFontGetMediumBold(), x, y, cPtr, color)
-    // }
+    public func drawString(text: String, x: Int32, y: Int32, color: Int32) {
+        let cString = text.cString(using: NSASCIIStringEncoding)!
+        let cPtr = UnsafeMutablePointer<UInt8>(cString)
+        gdImageString(im, gdFontGetMediumBold(), x, y, cPtr, color)
+    }
 
-    // public func pngPtr() -> (C7.Data, Int32) {
-    //     var size: Int32 = 0
-    //     let p = gdImagePngPtr(im, &size)
-    //     let buffer = UnsafeBufferPointer<Byte>(start: UnsafePointer(p), count: Int(size))
-    //     let bytes = Array<Byte>(buffer)
-    //     let data = Data(bytes)
-    //     gdFree(p)
-    //     return (data, size)
-    // }
+    public func pngPtr() -> ([Byte], Int32) {
+        var size: Int32 = 0
+        let p = gdImagePngPtr(im, &size)
+        let buffer = UnsafeBufferPointer<Byte>(start: UnsafePointer(p), count: Int(size))
+        let bytes = Array<Byte>(buffer)
+        gdFree(p)
+        return (bytes, size)
+    }
 
-    // deinit {
-    //     gdImageDestroy(im)
-    // }
+    deinit {
+        gdImageDestroy(im)
+    }
     
 }
